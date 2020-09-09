c2cf78f80921b9e5cbfa46e7042809f85fe4e935
noko1024 <44843376+noko1024@users.noreply.github.com>
2020-09-10 07:51:37 +0900

    Add file and update

--- Main.py
+++ Main.py
@@ -1,39 +1,44 @@
-#モジュールのimport準備
+
+#システム全般にかかわるライブラリ
 import os
 import sys
-
-#Main.pyまでのフルパスを取得し__file__でsplit
-basepath = os.path.split(os.path.realpath(__file__))[0]
-#ライブラリを保存したディレクトリを指定し有効化
-sys.path.insert(0, os.path.join(basepath, 'backlib')
-
-#システム全般に関わるライブラリ
 import threading
-import pathlib
 import json
+import pathlib
 import sqlite3
-#https://ymgsapo.com/2019/08/05/cryptography-python/#i
-import cryptography
-#import pycrypto
-#エラー音とか？
-#import pygame
+basepath = os.path.split(os.path.realpath(__file__))[0]
+
 #外部通信に関わるライブラリ
 #import bs4
 import requests
 
-#kivyに関するライブラリ
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.core.window import Window
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.core.window import Window
-from kivy.uix.widget import Widget
+import tkinter
+#PyQt
+import PyQt5
+from PyQt5 import QtCore,QtGui,uic,QtWidgets
+sys.path.insert(0, os.path.join(basepath, 'data/ui',))
+import proxyUI
+#Main.pyまでのフルパスを取得し__file__でsplit
+basepath = os.path.split(os.path.realpath(__file__))[0]
+#ライブラリを保存したディレクトリを指定し有効化
+sys.path.insert(0, os.path.join(basepath, 'data/lib',))
+
+#システム全般に関わるライブラリ
+#https://ymgsapo.com/2019/08/05/cryptography-python/#i
+import cryptography
+
+
+#最悪省いて良い機能
+#ユーザー認証(ログイン時),
+
+#ロードマップ
+#図書データCall機能,図書データ一覧表示機能,削除機能,proxy認証
+
 
 proxyInstance = "incetanceData"
-
+threadFlag = False
+tk = tkinter.Tk()
+tk.geometry("400x400")
 
 #kivyLangを引数化して制御したい。
 
@@ -45,64 +50,114 @@ proxyInstance = "incetanceData"
 #すべての変数をMainAppの管理下に置く必要はない。
 #あくまでも呼び出しを行うだけ。
 
-class MainApp(App):
+"""
+class MainApp():
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
+"""
 
 #ユーザーからの結果をreturnしたい
 #initで実行できない。
-#インスタンス.run()は何が帰る？
+
 #返したいものAPIConnのインスタンス
 
 #https://pyky.github.io/kivy-doc-ja/guide/basic.html
 #https://qiita.com/penta2019/items/a500630608960752a914
 
-#
-class ProxyRequestApp(App,Widget):
+#D:\Language\Python\Scripts\pyuic5.exe -x  --output=proxyUI.py proxy.ui
+
+
+
+class ProxyRequestApp(proxyUI.Ui_Dialog):
+    
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        self.instance = None
-    
-    def build(self):
-        Window.size = (400, 200)
-        return 
+        self.instance = proxyInstance
 
-    def activateButtonClicked(self):
+        self.proxyUse = False
+
+        self.app = QtWidgets.QApplication(sys.argv)
+        self.Dialog = QtWidgets.QDialog()
+        self.setupUi(self.Dialog)
+        self.Dialog.show()
+        print(self.app.exec_())
+        
+    
+
+    def ProxyStatus(self,Flag):
+        #トグルスイッチ毎に呼び出し
+        
+        self.proxyUse = Flag
+        self.InputID.text()
+        
+
+    def OKBtnPush(self):
         global proxyInstance
-        print(type(self.ids.text1.text))
-        proxyInstance = APICall(False)
-        proxyChk_Thread = threading.Thread(target=proxyInstance.connectionChk())
-        userStandby_Thread = threading.Thread(target=StandbyApp().run())
-        proxyChk_Thread.start()
-        userStandby_Thread.start()
-        proxyChk_Thread.join()
-        userStandby_Thread.join()
-    
-    def run(self,**kwargs):
-        super().run(**kwargs)
+        global threadFlag
 
-        return "PIP",self.instance
+        if self.proxyUse is True:
+            proxyInstance = APICall(True)
+        proxyInstance = APICall(False)
+        Standby = StandbyApp()
+        Thread = threading.Thread(target=proxyInstance.connectionChk)
+        Thread.start()
+        StandbyApp().show()
+        self.Dialog.close()
+
+
+
+"""
+        proxyInstance = APICall(False)
+        Standby = StandbyApp()
+        userStandby_Thread = threading.Thread(target=Standby.show)
+
+        print("SS")
+        userStandby_Thread.start()
+        print("SSS")
+        proxyInstance.connectionChk()
+        print("END")
+
+        Standby.Flag = True
+        print("EX"+str(threadFlag)) 
+        self.Dialog.close()
+"""
+
+
 
 #起動時パスワード認証
-class AuthRequestApp(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-    
+class AuthRequestApp():
+    def __init__(self):
+        pass
+
+
+class AuthInputApp():
+    def __init__(self):
+        pass
+
 
 #汎用的にしたい
-class StandbyApp(App):
+class StandbyApp():
 #resonとか表示させる?いつ止める？
 # →スレッドで回してjoinさせる？
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
+    def __init__(self):
+        pass
+    def show(self):
 
-    def build(self):
-        return
+        def ExitObserver():
+            tk.after(500,ExitObserver)
+            if threadFlag is True:
+                tk.destroy()
+                return
+
+        ExitObserver()
+        tk.mainloop()
+    
 
 
-class ErrorApp(App):
-    pass
+class ErrorApp():
+    def __init__(self):
+        pass
+
 
 #設定ファイル(config.json),DB(books.db)に決め打ち
 class FileIO:
@@ -110,7 +165,6 @@ class FileIO:
         self.basePath = basepath
         self.configPath = os.path.join(basepath,"config.json")
         self.dbPath = os.path.join(basepath,"books.db")
-    
 
     def existsChk(self):
         return os.path.exists(self.configPath),os.path.exists(self.dbPath)
@@ -128,9 +182,6 @@ class FileIO:
                 for key in keys:
                     pass
         
-        #ファイルが存在しない時
-        except FileNotFoundError:
-            pass
         #ファイルにアクセスできない時
         except:
             pass
@@ -140,9 +191,22 @@ class FileIO:
     def configWrite(self,key,value):
         pass
 
+    #ID(id) int,作品名(title) txt,出版社名(publisher) txt,発行日(issueDate) int 入架日(addStackDate) int,ジャンル  
     def dbInit(self):
-        pass        
-
+        conn = sqlite3.connect("Book.db")
+        c = conn.curser()
+        c.execute("CREATE TABLE books(id int,title text,writer text,publisher text,issueDate int,addStackDate int,BN-group int,BN-all)")
+        conn.commit()
+        conn.close()
+    
+    def dbadd(self,title,publisher,issue,):
+        conn = sqlite3.connect("Book.db")
+        c = conn.cursor()
+    
+    def dbread(self,id):
+        conn = sqlite3.connect("Book.db")
+        c = conn.cursor()
+        c.execute("select id from id where id = ?,")
 
 class APICall:
     def __init__(self,proxyFlag,ProxyID=None,ProxyPASS=None,ProxyIP=None,ProxyPort=None):
@@ -161,37 +225,50 @@ class APICall:
             "http":"http://%s:%s@%s:%s" % (self.ID,self.PASS,self.IP,self.PORT)
             }
     def connectionChk(self):
-        response = requests.get("https://ctf.noko1024.net/")
-        print(response.text)
-        print(response.status_code)
+        try:
+            response = requests.get("https://ctf.noko1024.net/")
+            print(response.text)
+            print(response.status_code)
+        except:
+            pass
+        finally:
+            print("EX")
+            global threadFlag
+            threadFlag = True
+            return
     #通信成功の可否とHTMLステータスコード,コンテンツを返す。
         return response.status_code,response.text
 
 
+    def AllCall(self,APIFlag,query):
+        pass
+
     
     #asyncio使いたい→スレッドになりそう。
     #https://note.crohaco.net/2019/python-asyncio/
+    
+    ##次回以降のアップデートで追加## 
     def ndlCall(self,query):
             maximum ="10"
             URL ="https://iss.ndl.go.jp/api/sru?operation=searchRetrieve&maximumRecords="+maximum+"&query=" +query.encode("utf-8")
             
-            response = requests.get(url,proxies=proxies)
-
+            response = requests.get(URL,proxies=proxies)
+    #############################
 
 #プログラム全体の初期化とユーザーへの情報要求
 #起動時にパスワード求めてフレーズを基にAESを復号→文字列切り出し＆演算で成功したか確認
 def init():
+    #StandbyApp()
     #プロキシの要求→(接続の確認)→ファイルの存在確認→設定ファイルの読み込み(or生成)→retunでAPIのインスタンスを返す
     IO = FileIO()
     config,db = IO.existsChk()
     if config is False:
         print("import TH")
-        ProxyRequestApp().run()
+        ProxyRequestApp()
 
-        print("return="+str(initProxy.instance))
     else:
         auth = IO.configRead(["Proxy"])
-        AuthRequestApp().run()
+        AuthRequestApp().show
         
     
     return
@@ -200,7 +277,9 @@ def init():
 #プログラム開始の起点
 def main():
     #App().run()
-    UI()
+    #UI()
+    pass
 
 if __name__ == "__main__":
-    main(init())
+    init()
+    main()
---
+++ data/ui/proxyUI.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'proxy.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        self.Dialog = Dialog
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(601, 228)
+        self.ProxyEnableButton = QtWidgets.QRadioButton(Dialog)
+        self.ProxyEnableButton.setGeometry(QtCore.QRect(10, 100, 145, 16))
+        self.ProxyEnableButton.setObjectName("ProxyEnableButton")
+        self.ProxyDisableButton = QtWidgets.QRadioButton(Dialog)
+        self.ProxyDisableButton.setGeometry(QtCore.QRect(10, 50, 151, 16))
+        self.ProxyDisableButton.setAutoRepeatDelay(300)
+        self.ProxyDisableButton.setObjectName("ProxyDisableButton")
+        self.OKButton = QtWidgets.QPushButton(Dialog)
+        self.OKButton.setGeometry(QtCore.QRect(480, 200, 75, 23))
+        self.OKButton.setObjectName("OKButton")
+        self.InputIP = QtWidgets.QLineEdit(Dialog)
+        self.InputIP.setGeometry(QtCore.QRect(20, 200, 121, 20))
+        self.InputIP.setInputMethodHints(QtCore.Qt.ImhHiddenText|QtCore.Qt.ImhNoAutoUppercase|QtCore.Qt.ImhNoPredictiveText|QtCore.Qt.ImhSensitiveData)
+        self.InputIP.setInputMask("")
+        self.InputIP.setText("")
+        self.InputIP.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.InputIP.setObjectName("InputIP")
+        self.InputPort = QtWidgets.QLineEdit(Dialog)
+        self.InputPort.setGeometry(QtCore.QRect(170, 200, 121, 20))
+        self.InputPort.setText("")
+        self.InputPort.setObjectName("InputPort")
+        self.InputPassword = QtWidgets.QLineEdit(Dialog)
+        self.InputPassword.setGeometry(QtCore.QRect(170, 150, 121, 20))
+        self.InputPassword.setText("")
+        self.InputPassword.setObjectName("InputPassword")
+        self.InputID = QtWidgets.QLineEdit(Dialog)
+        self.InputID.setGeometry(QtCore.QRect(20, 150, 121, 20))
+        self.InputID.setText("")
+        self.InputID.setObjectName("InputID")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(20, 130, 50, 12))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(170, 130, 50, 12))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(20, 180, 50, 12))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(170, 180, 50, 12))
+        self.label_4.setObjectName("label_4")
+
+        self.retranslateUi(Dialog)
+        self.OKButton.clicked.connect(self.OKBtnPush)
+        self.ProxyDisableButton.clicked.connect(lambda:self.ProxyStatus(False))
+        self.ProxyEnableButton.clicked.connect(lambda:self.ProxyStatus(True))
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.ProxyEnableButton.setText(_translate("Dialog", "プロキシ認証を使用する。"))
+        self.ProxyDisableButton.setText(_translate("Dialog", "プロキシ認証を使用しない"))
+        self.OKButton.setText(_translate("Dialog", "OK"))
+        self.label.setText(_translate("Dialog", "ログインID"))
+        self.label_2.setText(_translate("Dialog", "パスワード"))
+        self.label_3.setText(_translate("Dialog", "IPアドレス"))
+        self.label_4.setText(_translate("Dialog", "Port番号"))
+    
+    #override用
+    def OKBtnPush(self):
+        pass
+    def ProxyStatus(self,Flag):
+        
+        pass
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
